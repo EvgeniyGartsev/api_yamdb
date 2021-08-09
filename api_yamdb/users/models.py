5edd9946f9edb2a6cae29baf371f2f1ca361714a
Evgeniy <icancan1414@gmail.com>
2021-08-09 21:49:43 +0300

    Создал модель user

--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -1,5 +1,9 @@
+from django.db import models
 from django.contrib.auth.models import AbstractUser
 
-
+# нужно в модель добавить био и роль и сделать их choice
 class User(AbstractUser):
-    pass
+    bio = models.TextField(reqired=False)
+    role = models.CharField(max_length=30, required=False)
+
+    REQUIRED_FIELDS = ('username', 'email')
