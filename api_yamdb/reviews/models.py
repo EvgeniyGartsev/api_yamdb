9ca6389b68bc49093f9b55b2b9366b067aa6d22a
Liliana <liliyana.piatran@gmail.com>
2021-08-16 22:33:10 +0300

    Added db_index to reviews models

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -6,15 +6,18 @@ from users.models import User
 
 class Review(models.Model):
     title = models.ForeignKey(Title, on_delete=models.CASCADE,
-                              related_name='reviews')
-    text = models.TextField(verbose_name='Отзыв', help_text='Напишите отзыв')
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+                              related_name='reviews', db_index=True)
+    text = models.TextField(verbose_name='Отзыв', help_text='Напишите отзыв',
+                            db_index=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE,
+                               db_index=True)
     pub_date = models.DateTimeField(auto_now_add=True)
     score = models.IntegerField(validators=[MinValueValidator(1),
-                                            MaxValueValidator(10)])
+                                            MaxValueValidator(10)],
+                                db_index=True)
 
     class Meta:
-        ordering = ('-pub_date',)
+        ordering = ('-pub_date', 'score')
         constraints = [
             models.UniqueConstraint(fields=['title', 'author'],
                                     name='unique review')
@@ -23,11 +26,14 @@ class Review(models.Model):
 
 class Comment(models.Model):
     review = models.ForeignKey(Review, on_delete=models.CASCADE,
-                               related_name='comments')
+                               related_name='comments',
+                               db_index=True)
     text = models.TextField(verbose_name='Комментарий',
-                            help_text='Введите текст комментария')
+                            help_text='Введите текст комментария',
+                            db_index=True)
     pub_date = models.DateTimeField(auto_now_add=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE,
+                               db_index=True)
 
     class Meta:
         ordering = ('-pub_date',)
